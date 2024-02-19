f1e0c4c8db6c53542866342c78c9444c8c35bd71
chanbunheng <scbunheng.office@gmail.com>
2024-02-19 13:30:36 +0700

    update js detail

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiUrl = "https://restcountries.com/v3.1/all";
 let currentPage = 1;
 let countriesData;
 
-$(document).ready(function() {
+$(document).ready(function () {
     // Fetch data from the API
     fetch(apiUrl)
         .then(response => response.json())
@@ -25,19 +25,23 @@ function displayCountries(countries) {
 
     visibleCountries.forEach(country => {
         const card = $('<div class="country-div">')
-            .data('country', country) // Use jQuery's data method to store JSON data
+            .data('country', country)
             .append(`<img src="${country.flags.png}" alt="Flag">`)
             .append(`<h2>${country.name.common}</h2>`)
             .append(`<hr>`)
             .append(`<p>Population: ${country.population}</p>`)
             .append(`<p>Region: ${country.region}</p>`)
             .append(`<p>Capital: ${country.capital.join(', ')}</p>`)
-            .append(`<button class="details-btn">Details</button>`);
-    
+            .append(`<button class="details-btn">Details</button>`)
+            .click(function () {
+                const countryData = $(this).data('country');
+                displayDetailsModal(countryData);
+            });
+
         cardList.append(card);
     });
-    
-    
+
+
 
     // Update pagination information
     const totalPages = Math.ceil(countries.length / 25);
@@ -46,7 +50,7 @@ function displayCountries(countries) {
 
 function addEventListeners() {
     // Search functionality
-    $('#searchBtn').click(function() {
+    $('#searchBtn').click(function () {
         const searchTerm = $('#searchInput').val().toLowerCase();
         const filteredCountries = countriesData.filter(country => country.name.common.toLowerCase().includes(searchTerm));
         currentPage = 1; // Reset to the first page after searching
@@ -54,21 +58,21 @@ function addEventListeners() {
     });
 
     // Sorting functionality
-    $('#sortBtn').click(function() {
+    $('#sortBtn').click(function () {
         const sortType = $('#sort').val();
         const sortedCountries = sortCountries(countriesData, sortType);
         displayCountries(sortedCountries);
     });
 
     // Pagination functionality
-    $('#prevBtn').click(function() {
+    $('#prevBtn').click(function () {
         if (currentPage > 1) {
             currentPage--;
             displayCountries(countriesData);
         }
     });
 
-    $('#nextBtn').click(function() {
+    $('#nextBtn').click(function () {
         const totalPages = Math.ceil(countriesData.length / 25);
         if (currentPage < totalPages) {
             currentPage++;
@@ -77,19 +81,20 @@ function addEventListeners() {
     });
 
     // Modal functionality
-    $('.country-div').click(function() {
+    $('.country-div').click(function () {
         const countryData = $(this).data('country');
         displayModal(countryData);
     });
-    
 
-    // Close modal
-    $('.close').click(function() {
-        $('#modal').hide();
+
+    // Close details modal
+    $('#detailsModalClose').click(function () {
+        $('#detailsModal').hide();
     });
 
+
     // Details button in modal
-    $('#modalContent').on('click', '.details-btn', function() {
+    $('#modalContent').on('click', '.details-btn', function () {
         // Extract country data from the modal content
         const countryData = JSON.parse($('#modalContent').attr('data-country'));
         displayDetailsModal(countryData);
@@ -109,25 +114,6 @@ function sortCountries(countries, sortType) {
     });
 }
 
-function displayModal(countryData) {
-    const modalContent = $('#modalContent');
-    modalContent.empty();
-
-    // Display additional information in the modal
-    modalContent.append(`<h2>${countryData.name.common}</h2>`);
-    modalContent.append(`<img src="${countryData.flags.png}" alt="Flag">`);
-    modalContent.append(`<p><strong>Capital:</strong> ${countryData.capital.join(', ')}</p>`);
-    modalContent.append(`<p><strong>Population:</strong> ${countryData.population.toLocaleString()}</p>`);
-    modalContent.append(`<p><strong>Region:</strong> ${countryData.region}</p>`);
-    modalContent.append(`<p><strong>Subregion:</strong> ${countryData.subregion}</p>`);
-    modalContent.append(`<button class="details-btn">Details</button>`);
-
-    // Set data-country attribute for details button
-    modalContent.attr('data-country', JSON.stringify(countryData));
-
-    // Show modal
-    $('#modal').show();
-}
 
 function displayDetailsModal(countryData) {
     const detailsModalContent = $('#detailsModalContent');
@@ -136,20 +122,44 @@ function displayDetailsModal(countryData) {
     try {
         // Ensure that countryData is a valid JSON string
         const parsedData = JSON.parse(JSON.stringify(countryData));
+        const nativeNames = parsedData.name.nativeName;
+        const languages = parsedData.languages;
+        const currencies = parsedData.currencies;
 
         // Display detailed information in the details modal
         detailsModalContent.append(`<h2>${parsedData.name.common}</h2>`);
         detailsModalContent.append(`<img src="${parsedData.flags.png}" alt="Flag">`);
         detailsModalContent.append(`<p><strong>Official Name:</strong> ${parsedData.name.official}</p>`);
-        detailsModalContent.append(`<p><strong>Native Name:</strong> ${parsedData.name.nativeName}</p>`);
-        detailsModalContent.append(`<p><strong>Alternative Names:</strong> ${parsedData.altSpellings.join(', ')}</p>`);
+        if (nativeNames) {
+            Object.keys(nativeNames).forEach(lang => {
+                const officialName = nativeNames[lang].official;
+                detailsModalContent.append(`<p><strong>Native Name (${lang}):</strong> ${officialName}</p>`);
+            });
+        } else {
+            detailsModalContent.append(`<p><strong>Native Name:</strong> N/A</p>`);
+        } detailsModalContent.append(`<p><strong>Alternative Names:</strong> ${parsedData.altSpellings.join(', ')}</p>`);
         detailsModalContent.append(`<p><strong>Country Codes:</strong> ${parsedData.cca2} (2 characters), ${parsedData.cca3} (3 characters)</p>`);
         detailsModalContent.append(`<p><strong>Country Calling Code:</strong> +${parsedData.cca2}</p>`);
         detailsModalContent.append(`<p><strong>Region:</strong> ${parsedData.region}</p>`);
         detailsModalContent.append(`<p><strong>Subregion:</strong> ${parsedData.subregion}</p>`);
         detailsModalContent.append(`<p><strong>Area:</strong> ${parsedData.area.toLocaleString()} square kilometers</p>`);
-        detailsModalContent.append(`<p><strong>Official Languages:</strong> ${parsedData.languages.official.join(', ')}</p>`);
-        detailsModalContent.append(`<p><strong>Currencies:</strong> ${Object.values(parsedData.currencies).join(', ')}</p>`);
+        if (languages) {
+            Object.keys(languages).forEach(lang => {
+                const language = languages[lang];
+                detailsModalContent.append(`<p><strong>Official Language (${lang}):</strong> ${language}</p>`);
+            });
+        } else {
+            detailsModalContent.append(`<p><strong>Official Languages:</strong> N/A</p>`);
+        }
+        if (currencies) {
+            Object.keys(currencies).forEach(currencyCode => {
+                const currency = currencies[currencyCode];
+                detailsModalContent.append(`<p><strong>Currency (${currencyCode}):</strong> ${currency.name} (${currency.symbol})</p>`);
+            });
+        } else {
+            detailsModalContent.append(`<p><strong>Currencies:</strong> N/A</p>`);
+        }
+
     } catch (error) {
         console.error('Error parsing countryData:', error);
         detailsModalContent.append('<p>Error parsing country data.</p>');
